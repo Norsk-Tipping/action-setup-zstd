8d25fd2c5d649f2be3da7759ec7fbcba0cba3911
Erik Weber <erik.weber@norsk-tipping.no>
2022-12-14 14:38:28 +0100

    hard coded path

--- src/index.js
+++ src/index.js
@@ -6,28 +6,17 @@ const zstdVersion = '1.5.2'
 
 async function setup() {
     try {
-        let myOutput = ''
-        let myError = ''
-        /* const options= {}
-        options.listeners = {
-            stdout: (data) => {
-                myOutput += data.toString();
-            },
-            stderr: (data) => {
-                myError += data.toString()
-            }
-        };
- */
-        await exec.exec('pwd')
-        await exec.exec('find / -name zstd')
-        console.log(`myOutput: ${myOutput}`)
-        console.log(`myError: ${myError}`)
+
+        // debug stuff to find binary within action
+        /* await exec.exec('pwd')
+        await exec.exec('find / -name zstd') */
+
         let toolPath = tc.find('zstd', zstdVersion)
         if (toolPath) {
             core.info(`Found zstd in tool cache at ${toolPath}`)
         } else {
             core.info(`zstd not found in tool cache, adding file`)
-            toolPath = await tc.cacheFile('dist/zstd', 'zstd', 'zstd', zstdVersion )
+            toolPath = await tc.cacheFile('/home/runner/_work/_actions/Norsk-Tipping/action-setup-zstd/main/dist/zstd', 'zstd', 'zstd', zstdVersion )
         }
         core.addPath(toolPath)
         core.info(`zstd available at ${toolPath}`)
