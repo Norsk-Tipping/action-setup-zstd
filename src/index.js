ab880b7a1bae9b7431d5a04fab7764d23eeea355
Erik Weber <erik.weber@norsk-tipping.no>
2022-12-14 15:12:55 +0100

    Add zsdtmt to tools list

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 const core = require('@actions/core')
 const tc = require('@actions/tool-cache')
-const exec = require('@actions/exec')
 
 const zstdVersion = '1.5.2'
+const tools = ['zsdt', 'zstdmt']
 
 async function setup() {
-    try {
 
-        // debug stuff to find binary within action
-        /* await exec.exec('pwd')
-        await exec.exec('find / -name zstd') */
-
-        let toolPath = tc.find('zstd', zstdVersion)
-        if (toolPath) {
-            core.info(`Found zstd in tool cache at ${toolPath}`)
-        } else {
-            core.info(`zstd not found in tool cache, adding file`)
-            toolPath = await tc.cacheFile('/home/runner/_work/_actions/Norsk-Tipping/action-setup-zstd/main/dist/zstd', 'zstd', 'zstd', zstdVersion )
+    // debug stuff to find binary within action
+    /* await exec.exec('pwd')
+    await exec.exec('find / -name zstd') */
+    tools.forEach(async function (toolName) {
+        try {
+            let toolPath = tc.find(toolName, zstdVersion)
+            if (toolPath) {
+                core.info(`Found ${toolName} in tool cache at ${toolPath}`)
+            } else {
+                core.info(`${toolName} not found in tool cache, adding file`)
+                toolPath = await tc.cacheFile(`/home/runner/_work/_actions/Norsk-Tipping/action-setup-zstd/main/dist/${toolName}`, toolName, toolName, zstdVersion )
+            }
+            core.addPath(toolPath)
+            core.info(`${toolName} available at ${toolPath}`)
+        } catch (error) {
+            core.setFailed(error)
         }
-        core.addPath(toolPath)
-        core.info(`zstd available at ${toolPath}`)
-    } catch (error) {
-        core.setFailed(error)
-    }
+    })
 }
 module.exports = setup
 
